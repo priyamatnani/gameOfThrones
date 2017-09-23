bd6b3686d17c45fc7875b49f02435cf91ca420e6
kartikn27 <kanagre@syr.edu>
2017-09-23 16:27:46 -0400

    Changes to model and import battle data script

--- backend/got/import_battles.py
+++ backend/got/import_battles.py
@@ -2,39 +2,39 @@ from battles.models import BattleSet
 from itertools import islice
 import csv
 
-csv_file_name = '/Users/priya/Projects/kaggle_dataset/GOT_dataset/game-of-thrones/battles.csv'
-data_reader = csv.reader(open(csv_file_name), delimeter=',', quotechar='"')
+csv_file_name = '/Users/kartik/Documents/kaggle_datasets/game_of_thrones/game-of-thrones/battles1.csv'
+data_reader = csv.reader(open(csv_file_name), quotechar='"')
 
 
 def write_to_table(start_index, end_index):
     bulk_data = []
     for col in islice(data_reader, start_index, end_index):
         battle = BattleSet()
-        battle.name = col[0]
-        battle.year = col[1]
-        battle.battle_number = col[2]
-        battle.attacker_king = col[3]
-        battle.defender_king = col[4]
-        battle.attacker_1 = col[5]
-        battle.attacker_2 = col[6]
-        battle.attacker_3 = col[7]
-        battle.attacker_4 = col[8]
-        battle.defender_1 = col[9]
-        battle.defender_2 = col[10]
-        battle.defender_3 = col[11]
-        battle.defender_4 = col[12]
-        battle.attacker_outcome = col[13]
-        battle.battle_type = col[14]
-        battle.major_death = col[15]
-        battle.major_capture = col[16]
-        battle.attacker_size = col[17]
-        battle.defender_size = col[18]
-        battle.attacker_commander = col[19]
-        battle.defender_commander = col[20]
-        battle.summer = col[21]
-        battle.location = col[22]
-        battle.region = col[23]
-        battle.note = col[24]
+        battle.name = str(col[0])
+        battle.year = int(col[1]) if col[1] else None        
+        battle.battle_number = int(col[2]) if col[2] else None
+        battle.attacker_king = str(col[3])
+        battle.defender_king = str(col[4])
+        battle.attacker_1 = str(col[5])
+        battle.attacker_2 = str(col[6])
+        battle.attacker_3 = str(col[7])
+        battle.attacker_4 = str(col[8])
+        battle.defender_1 = str(col[9])
+        battle.defender_2 = str(col[10])
+        battle.defender_3 = str(col[11])
+        battle.defender_4 = str(col[12])
+        battle.attacker_outcome = str(col[13])
+        battle.battle_type = str(col[14])
+        battle.major_death = int(col[15]) if col[15] else None
+        battle.major_capture = int(col[16]) if col[16] else None
+        battle.attacker_size = int(col[17]) if col[17] else None
+        battle.defender_size = int(col[18]) if col[18] else None
+        battle.attacker_commander = str(col[19])
+        battle.defender_commander = str(col[20])
+        battle.summer = int(col[21]) if col[21] else None
+        battle.location = str(col[22])
+        battle.region = str(col[23])
+        battle.note = str(col[24])
         bulk_data.append(battle)
     BattleSet.objects.bulk_create(bulk_data)
 

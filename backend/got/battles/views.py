eb2fc6f4c9babe29425a2e7075f18a1e0cea49f2
priyamatnani <matnanipriya@gmail.com>
2017-09-24 20:32:57 -0400

    battleset api no output

--- backend/got/battles/views.py
+++ backend/got/battles/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from battles.models import BattleSet
+import json
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def battle_details(request):
+
+    #battle_list = []
+    success = True
+    query = request.GET.get('q')
+    print("query ", query)
+    print("ajbvskjb ", BattleSet.objects.filter(year=query).exists())
+    if query:
+        if BattleSet.objects.filter(year=query).exists():
+            battle_list = list(BattleSet.objects.filter(year=query))  # contains
+            print(">>>>>>>>>>>>>>",battle_list)
+        else:
+            success = False
+
+    return Response({"result": json.dumps(battle_list), "success": success})
+
--- backend/got/got/urls.py
+++ backend/got/got/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from battles import views as battle_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^battleDetailsDisplay/$', battle_views.battle_details)
 ]
+
+
